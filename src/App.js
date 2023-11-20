ebdd6b956415374e04c1a25ceb2190c4123ac331
Mohanty <sourav.c.mohanty@accenture.com>
2023-11-20 10:43:17 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/Header';
 import WeatherDisplay from './components/WeatherDisplay';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <WeatherDisplay/>
     </div>
   );
